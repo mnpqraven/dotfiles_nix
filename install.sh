9420ac64da6aebad19846a282a61b00b18bb74b1
Othi <mnpq.raven@gmail.com>
2025-03-31 22:46:39 +0700

    feat: eww conf

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ ln -s ~/dotfiles_nix/.config/home-manager ~/.config/home-manager
 mkdir -p ~/.config/hypr
 ln -sf ~/dotfiles_nix/.config/hypr/hyprland.conf ~/.config/hypr
 ln -sf ~/dotfiles_nix/.config/wpaperd  ~/.config/wpaperd
+ln -sf ~/dotfiles_nix/.config/eww  ~/.config/eww
