123ab3cc8e181da759f5079f06f44c1bba3697c7
Othi <mnpq.raven@gmail.com>
2025-04-09 10:16:31 +0700

    basic (incomplete) keymap

--- home/editor/helix.nix
+++ home/editor/helix.nix
@@ -1,24 +1,12 @@
 _: {
   programs.helix = {
     enable = true;
-    # settings = {
-    #   theme = "autumn_night_transparent";
-    #   editor.cursor-shape = {
-    #     normal = "block";
-    #     insert = "bar";
-    #     select = "underline";
-    #   };
-    # };
-    # languages.language = [{
-    #   name = "nix";
-    #   auto-format = true;
-    #   formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
-    # }];
-    # themes = {
-    #   autumn_night_transparent = {
-    #     "inherits" = "autumn_night";
-    #     "ui.background" = { };
-    #   };
-    # };
+    languages.language = [
+      # {
+      #   name = "nix";
+      #   auto-format = true;
+      #   formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+      # }
+    ];
   };
 }
