1a3d4735e82a2c9542bb4899ffd66a8fe837610d
Othi <mnpq.raven@gmail.com>
2025-04-10 14:06:39 +0700

    compile

--- home/editor/nvim/oil.nix
+++ home/editor/nvim/oil.nix
@@ -1,5 +1,13 @@
 _: {
-  programs.nvf.settings.vim.filetree.oil = {
+  programs.nvf.settings.vim.keymaps = [
+    {
+      desc = "Oil panel";
+      key = "<leader>tf";
+      action = ":Oil --float<CR>";
+      mode = ["n"];
+    }
+  ];
+  programs.nvf.settings.vim.utility.oil-nvim = {
     enable = true;
   };
 }
