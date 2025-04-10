ea7419e6a16949ef9be26c63b2ad8237f26cbfb1
Othi <mnpq.raven@gmail.com>
2025-04-10 12:44:47 +0700

    helix configuration impl (#46)
    * initial
    
    * basic (incomplete) keymap
    
    * # This is a combination of 3 commits.
    # This is the 1st commit message:
    
    dev: add basic toml, rust, ts LSPs (ts using biome)
    
    # This is the commit message #2:
    
    dev
    
    # This is the commit message #3:
    
    dev: trying biome in replace for eslint
    
    * dev: add basic toml, rust, ts LSPs (ts using biome)
    
    dev
    
    dev: trying biome in replace for eslint
    
    clean up code using eslint installation
    
    dev: add basic toml, rust, ts LSPs (ts using biome)
    
    dev
    
    dev: trying biome in replace for eslint
    
    clean up code using eslint installation
    
    * basic command
    
    * feat(helix): more keybinds

--- home/editor/helix.nix
+++ home/editor/helix.nix
@@ -1,24 +1,27 @@
-_: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  cfgPath = "${config.home.homeDirectory}/dotfiles_nix/.config/helix";
+in {
+  xdg.configFile."helix".source = config.lib.file.mkOutOfStoreSymlink cfgPath;
+
+  home.packages = with pkgs; [
+    # LSPs
+    nil
+    typescript-language-server
+    vscode-langservers-extracted
+    lua-language-server
+
+    # formatters
+    taplo
+    biome
+    prettierd
+    nixfmt-rfc-style
+  ];
+
   programs.helix = {
     enable = true;
-    # settings = {
-    #   theme = "autumn_night_transparent";
-    #   editor.cursor-shape = {
-    #     normal = "block";
-    #     insert = "bar";
-    #     select = "underline";
-    #   };
-    # };
-    # languages.language = [{
-    #   name = "nix";
-    #   auto-format = true;
-    #   formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
-    # }];
-    # themes = {
-    #   autumn_night_transparent = {
-    #     "inherits" = "autumn_night";
-    #     "ui.background" = { };
-    #   };
-    # };
   };
 }
