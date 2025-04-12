f3fa357a9fddc141f80e73cace7ae20c761966f1
Othi <mnpq.raven@gmail.com>
2025-04-12 21:23:33 +0700

    dev: merge hyprland dirs

--- home/xdgSymlinks.nix
+++ home/xdgSymlinks.nix
@@ -9,14 +9,7 @@ in {
   # TODO: different sources based on device platform
   xdg.configFile = {
     helix.source = symlink "${cfg}/helix";
-    hypr.source =
-      if inputs.device == "pc"
-      then symlink "${cfg}/hypr_pc"
-      else symlink "${cfg}/hypr";
-    "hypr.monitors.conf".source =
-      if symlink inputs.device == "pc"
-      then "${cfg}/hypr/monitors_pc.conf"
-      else symlink "${cfg}/hypr/monitors_laptop.conf";
+    hypr.source = symlink "${cfg}/hypr";
     macchina.source = symlink "${cfg}/macchina";
     "starship.toml".source = symlink "${cfg}/starship.toml";
     tofi.source = symlink "${cfg}/tofi";
