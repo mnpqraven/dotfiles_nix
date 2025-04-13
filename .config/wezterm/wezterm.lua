e7c4f3da33a03428e86b0fe5804c12dc194291f1
Othi <mnpq.raven@gmail.com>
2025-04-13 14:34:30 +0700

    feat(wezterm): conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "tokyonight"
-config.hide_tab_bar_if_only_one_tab = true
+config = {
+  color_scheme = "tokyonight",
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.8,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
 
 return config
