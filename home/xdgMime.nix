2bf8913d45619b07cb660b0df3392a3b81a4a1a0
Othi <mnpq.raven@gmail.com>
2025-04-14 17:06:38 +0700

    feat(helix): updated helix conf + add `nixfmt`

--- home/xdgMime.nix
+++ home/xdgMime.nix
@@ -2,13 +2,13 @@ _: {
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
     # Images
-    "image/*" = ["feh.desktop"];
-    "image/png" = ["feh.desktop"];
-    "image/jpeg" = ["feh.desktop"];
-    "text/html" = ["librewolf.desktop"];
-    "x-scheme-handler/http" = ["librewolf.desktop"];
-    "x-scheme-handler/https" = ["librewolf.desktop"];
-    "x-scheme-handler/about" = ["librewolf.desktop"];
-    "x-scheme-handler/unknown" = ["librewolf.desktop"];
+    "image/*" = [ "feh.desktop" ];
+    "image/png" = [ "feh.desktop" ];
+    "image/jpeg" = [ "feh.desktop" ];
+    "text/html" = [ "librewolf.desktop" ];
+    "x-scheme-handler/http" = [ "librewolf.desktop" ];
+    "x-scheme-handler/https" = [ "librewolf.desktop" ];
+    "x-scheme-handler/about" = [ "librewolf.desktop" ];
+    "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
   };
 }
