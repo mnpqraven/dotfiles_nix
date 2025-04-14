2170f21f248a82563bd0d1e71534c0cadd82d5fb
Othi <mnpq.raven@gmail.com>
2025-04-14 21:55:39 +0700

    feat(nas mountpoints): add mountpoints

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -10,7 +10,7 @@
     ./hardware-configuration.nix
     # PC needs NVIDIA configs as well
     ./nvidia.nix
-    ./nas.nix
+    ./mounts.nix
   ];
 
   # Bootloader.
--- hosts/pc/nas.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  systemd.mounts."nasmusic" = {
-    what = "//192.168.1.14/music";
-    where = "/media/nas/music";
-    type = "cifs";
-    options = "nobrl,_netdev,credentials=/etc/.credentials,uid=1000,gid=1000,users,rw";
-  };
-}
