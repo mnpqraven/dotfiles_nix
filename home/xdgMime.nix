ad265405e9b748e056ed5959c1c8a67a7c810262
Othi <mnpq.raven@gmail.com>
2025-04-14 16:56:18 +0700

    feat(mime): defaults to librewolf

--- home/xdgMime.nix
+++ home/xdgMime.nix
@@ -5,5 +5,10 @@ _: {
     "image/*" = ["feh.desktop"];
     "image/png" = ["feh.desktop"];
     "image/jpeg" = ["feh.desktop"];
+    "text/html" = ["librewolf.desktop"];
+    "x-scheme-handler/http" = ["librewolf.desktop"];
+    "x-scheme-handler/https" = ["librewolf.desktop"];
+    "x-scheme-handler/about" = ["librewolf.desktop"];
+    "x-scheme-handler/unknown" = ["librewolf.desktop"];
   };
 }
