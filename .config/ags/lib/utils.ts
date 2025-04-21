d1ab1a5b60cff12f1e21fd05e1398fcb1d2d8896
Othi <mnpq.raven@gmail.com>
2025-04-21 17:19:47 +0700

    feat(ags): move to own repo

--- .config/ags/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @param num float ranging from 0 to 1
- */
-export function percentage(num: number, fixedLen = 0): string {
-  const per = num * 100;
-  return per.toFixed(fixedLen) + " %";
-}
--- .config/ags/widget/CurrentWindowName.tsx
+++
@@ -1,9 +0,0 @@
-import { bind } from "astal";
-import AstalHyprland from "gi://AstalHyprland";
-
-export function CurrentWindowName() {
-  const hypr = AstalHyprland.get_default();
-  const windowBind = bind(hypr, "focusedClient").as((client) => client.title);
-
-  return <label cssClasses={["current-name"]} label={windowBind} />;
-}
