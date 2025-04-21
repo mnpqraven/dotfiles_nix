a4c898311fba7d4b946187188f5ca961c90be2dc
Othi <mnpq.raven@gmail.com>
2025-04-21 17:10:13 +0700

    chore(nix-ld): using full list

--- base/development.nix
+++ base/development.nix
@@ -1,7 +1,32 @@
 { pkgs, ... }:
 {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      zlib
+      zstd
+      stdenv.cc.cc
+      curl
+      openssl
+      attr
+      libssh
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     protobuf
+    # https://wiki.nixos.org/wiki/Python
+    # https://github.com/nix-community/nix-ld?tab=readme-ov-file#my-pythonnodejsrubyinterpreter-libraries-do-not-find-the-libraries-configured-by-nix-ld
+    (pkgs.writeShellScriptBin "python" ''
+      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+      exec ${pkgs.python310}/bin/python "$@"
+    '')
   ];
 }
