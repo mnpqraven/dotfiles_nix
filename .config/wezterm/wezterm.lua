dfe100be2f39f5eaf3deeb33e6ec1afe9e848d67
Othi <mnpq.raven@gmail.com>
2025-05-06 10:34:19 +0700

    feat(wezterm): add lua annotations

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,15 @@
+---@class Wezterm
 local wezterm = require("wezterm")
+
+---@type Config
 local config = wezterm.config_builder()
 
+require("keys").apply_to_config(config)
+
+---@diagnostic disable-next-line: missing-fields
 config = {
   color_scheme = "tokyonight",
-  font = wezterm.font_with_fallback { 'JetBrainsMono NFM', 'Source Han Code JP', 'Noto Color Emoji' },
+  font = wezterm.font_with_fallback({ "JetBrainsMono NFM", "Source Han Code JP", "Noto Color Emoji" }),
   font_size = 12,
   hide_tab_bar_if_only_one_tab = true,
   window_background_opacity = 0.8,
