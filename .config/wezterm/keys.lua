522bcc09e87d44661790ef8b256b9cb35d9b3451
Othi <mnpq.raven@gmail.com>
2025-05-06 15:23:47 +0700

    feat(lua): stylua configs + fmt

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -7,9 +7,9 @@ local module = {}
 
 ---@param config Config
 function module.config_mut(config)
-	config.keys = {
-		{ key = "t", mods = "SUPER|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
-	}
+  config.keys = {
+    { key = "t", mods = "SUPER|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+  }
 end
 
 return module
