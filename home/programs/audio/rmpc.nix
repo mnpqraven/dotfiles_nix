e666882eba1993beca4fb6a17129b7db564c1975
Othi <mnpq.raven@gmail.com>
2025-05-08 23:43:27 +0700

    bump

--- home/programs/audio/rmpc.nix
+++ home/programs/audio/rmpc.nix
@@ -15,8 +15,8 @@ let
             src = pkgs.fetchFromGitHub {
               owner = "mierak";
               repo = "rmpc";
-              rev = "f23ab14404bbf491de06819f3bdaf829cc0ba880";
-              hash = "sha256-Lxu9x+LBnNOxEFE2CcQ3yiji4t2B9OIy1TW4jPDTJBk=";
+              rev = "8daf632587669aa60cc1d6071625aa47bd39ca63";
+              hash = "sha256-m0/4hwSIE0p35uhyeS6kUaLUwvR5S52mM3Zqzlbizm0=";
             };
             cargoHash = "sha256-RQ0ibBFkVq7KgYrbMeIgTBSDJbCLCea/ThhbJgOuKxk=";
           }
