08d4f7175cd3997e7252acb20162df3db1227a5f
Othi <mnpq.raven@gmail.com>
2025-05-10 14:40:10 +0700

    feat(easyeffects): default presets

--- home/programs/audio/default.nix
+++ home/programs/audio/default.nix
@@ -1,5 +1,11 @@
-_: {
+{ config, ... }:
+let
+  symlink = config.lib.file.mkOutOfStoreSymlink;
+in
+{
   services.easyeffects.enable = true;
+  xdg.configFile."easyeffects/output".source =
+    symlink "${config.home.homeDirectory}/dotfiles_nix/.config/easyeffects/output";
   imports = [
     ./mpd.nix
     ./rmpc.nix
