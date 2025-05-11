df320b8111df7719d6ef22d4b0c85af35f983f83
Othi <mnpq.raven@gmail.com>
2025-05-11 14:20:00 +0700

    feat(easyeffects): fix symlink

--- home/programs/audio/default.nix
+++ home/programs/audio/default.nix
@@ -4,8 +4,12 @@ let
 in
 {
   services.easyeffects.enable = true;
-  xdg.configFile."easyeffects/output".source =
-    symlink "${config.home.homeDirectory}/dotfiles_nix/.config/easyeffects/output";
+  xdg.configFile.easyeffects = {
+    target = "${config.home.homeDirectory}/.config/easyeffect/output";
+    source = symlink "${config.home.homeDirectory}/dotfiles_nix/.config/easyeffects/output";
+    recursive = true;
+    force = true;
+  };
   imports = [
     ./mpd.nix
     ./rmpc.nix
