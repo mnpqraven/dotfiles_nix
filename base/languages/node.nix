8241e5efe9a1bd2117527b4965fd3385760455aa
Othi <mnpq.raven@gmail.com>
2025-06-19 15:06:41 +0700

    bump

--- base/languages/node.nix
+++ base/languages/node.nix
@@ -1,28 +1,5 @@
 { pkgs, ... }:
 {
-  # @see https://github.com/tailwindlabs/tailwindcss-intellisense/issues/1380
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     tailwindcss-language-server = prev.tailwindcss-language-server.overrideAttrs (old: rec {
-  #       src = prev.fetchFromGitHub {
-  #         owner = "tailwindlabs";
-  #         repo = "tailwindcss-intellisense";
-  #         rev = "v0.14.10";
-  #         hash = "sha256-QkidKyns36g+cUY3R20OKZyo0PIO3NOOUL6CnanvvTI=";
-  #       };
-  #       pnpmDeps = prev.pnpm_9.fetchDeps {
-  #         inherit (old)
-  #           pname
-  #           pnpmWorkspaces
-  #           prePnpmInstall
-  #           ;
-  #         inherit src;
-  #         version = "0.14.10";
-  #         hash = "sha256-ODP8HrUNs+0+9v+PDSm8N6t5ixpPoIxZ9Uw5MbdFVA0=";
-  #       };
-  #     });
-  #   })
-  # ];
   environment.systemPackages = with pkgs; [
     nodejs_20
     pnpm
