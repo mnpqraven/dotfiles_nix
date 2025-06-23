0f106434ed5c1d317810843a9153bfdb60be6fcd
Othi <mnpq.raven@gmail.com>
2025-06-23 19:16:56 +0700

    correct cursor

--- home/de/niri.nix
+++ home/de/niri.nix
@@ -19,12 +19,11 @@ lib.mkIf osConfig.features.desktops.niri.enable {
     target = "./niri/config.kdl";
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles_nix/.config/niri/${device}.kdl";
   };
-  # DEV: buggy as hell
   home.pointerCursor = {
+    name = "graphite-dark";
+    size = 32;
+    package = pkgs.graphite-cursors;
+    x11.enable = true;
     gtk.enable = true;
-    enable = true;
-    name = "Rose Pine Cursor";
-    package = pkgs.rose-pine-cursor;
-    size = 24;
   };
 }
