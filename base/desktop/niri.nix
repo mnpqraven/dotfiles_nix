0cf3b5cb08cd3ce54e195a90127c3c1c0bcb1d15
Othi <mnpq.raven@gmail.com>
2025-06-24 16:56:23 +0700

    feat(niri): add xwayland support + force electron to use x11

--- base/desktop/niri.nix
+++ base/desktop/niri.nix
@@ -1,4 +1,13 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 lib.mkIf config.features.desktops.niri.enable {
   programs.niri.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    xwayland-satellite
+  ];
 }
