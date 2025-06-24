888f1b1b3821aee165c18cd4eca021905cfac6d6
Othi <mnpq.raven@gmail.com>
2025-06-24 18:38:31 +0700

    deduplicate portal for niri

--- home/de/niri.nix
+++ home/de/niri.nix
@@ -9,12 +9,6 @@ let
   device = osConfig.networking.hostName;
 in
 lib.mkIf osConfig.features.desktops.niri.enable {
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-    ];
-  };
   xdg.configFile.niri = {
     target = "./niri/config.kdl";
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles_nix/.config/niri/${device}.kdl";
