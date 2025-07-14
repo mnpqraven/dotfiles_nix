723cbfee016d306cb5216fb4d4c2f8d7c4379a15
Othi <mnpq.raven@gmail.com>
2025-07-14 15:40:45 +0700

    add monitor map

--- home/de/wallthi.nix
+++ home/de/wallthi.nix
@@ -1,7 +1,33 @@
 {
   inputs,
+  osConfig,
+  config,
   ...
 }:
+let
+  device = osConfig.networking.hostName;
+  monitorMap = {
+    pc = {
+      "DP-1".resolution = "2560x1440";
+      "DP-2" = {
+        resolution = "1080x1920";
+        vertical = true;
+        transform = 90; # note: hyprland is 1
+      };
+      "DP-3".resolution = "1920x1080";
+    };
+    pcremote = {
+      "HDMI-A-1".resolution = "1920x1080";
+      "DP-1".resolution = "1920x1080";
+    };
+    laptop = {
+      "HDMI-A-1".resolution = "1920x1080";
+    };
+    homelab = { };
+  };
+  monitor = monitorMap.${device};
+
+in
 {
   imports = [
     inputs.wallthi.homeManagerModules.default
@@ -12,13 +38,11 @@
       general = {
         # TODO: by hosts
         duration = 60;
-        path = [ "/home/othi/wallpaper/horizontal" ];
-        path_vertical = [ "/home/othi/wallpaper/vertical" ];
+        path = [ "${config.home.homeDirectory}/wallpaper/horizontal" ];
+        path_vertical = [ "${config.home.homeDirectory}/wallpaper/vertical" ];
       };
 
-      monitor = {
-        "HDMI-A-1".resolution = "1920x1080";
-      };
+      inherit monitor;
     };
   };
 }
