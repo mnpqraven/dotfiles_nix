4e7947831ac45fda62466da7dbe730dc1b595dfb
Othi <mnpq.raven@gmail.com>
2025-08-05 15:58:57 +0700

    blanket sops conf

--- base/security.nix
+++ base/security.nix
@@ -1,3 +1,20 @@
-_: {
+{ config, ... }:
+{
   security.rtkit.enable = true;
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    age = {
+      # This will automatically import SSH keys as age keys
+      sshKeyPaths = [ "/home/othi/.ssh/id_ed25519" ];
+      # This is using an age key that is expected to already be in the filesystem
+      keyFile = "/home/othi/.config/sops/age/keys.txt";
+      # This will generate a new key if the key specified above does not exist
+      generateKey = true;
+    };
+
+    secrets = {
+      # This is the actual specification of the secrets.
+    };
+    templates = { };
+  };
 }
