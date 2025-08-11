57d89bd2e5bb7a8436afbd86cf3610d3cbda7e5d
Othi <mnpq.raven@gmail.com>
2025-08-11 09:52:04 +0700

    bump: dunst

--- home/de/notifications.nix
+++ home/de/notifications.nix
@@ -8,19 +8,6 @@
 lib.mkIf
   (osConfig.features.notifications.enable && osConfig.features.desktops.hyprland.bar == "waybar")
   {
-    nixpkgs.overlays = [
-      (final: prev: {
-        dunst = prev.dunst.overrideAttrs (old: {
-          src = prev.fetchFromGitHub {
-            owner = "bynect";
-            repo = "dunst";
-            rev = "f0506a0aaa770a4e1668e577539b729b97afbb0e";
-            hash = "sha256-uMeeKciPeB6zmDvs9yOH+MNsCHLQDpWRpzoc5+hnveI=";
-          };
-        });
-      })
-    ];
-
     home.packages = with pkgs; [
       dunst
     ];
