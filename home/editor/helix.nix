0fe560038e6176243f8eb7c9cb0b823361ab0406
Othi <mnpq.raven@gmail.com>
2025-11-06 10:11:18 +0700

    tss bump to avoid helix deadlock

--- home/editor/helix.nix
+++ home/editor/helix.nix
@@ -12,6 +12,19 @@ lib.mkIf osConfig.features.editors.helix.enable {
   # FIXME: dyn
   xdg.configFile.helix.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles_nix/.config/helix";
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      typescript-language-server = prev.typescript-language-server.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "typescript-language-server";
+          repo = "typescript-language-server";
+          rev = "3e9620951eb79f741e22131670192282baff240c";
+          hash = "sha256-wYpW/HHuMetxnwEgGeQ8ptT6Kd5wp2kqUXnjWSkoDQY=";
+        };
+      });
+    })
+  ];
+
   home.packages = with pkgs; [
     # nix
     nil
