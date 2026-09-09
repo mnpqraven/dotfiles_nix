0d9672910208f39b23a00b23ffb4269511a32f6d
Othi <mnpq.raven@gmail.com>
2026-09-10 01:40:50 +0700

    chore

--- base/desktop/niri.nix
+++ base/desktop/niri.nix
@@ -4,10 +4,25 @@
   pkgs,
   ...
 }:
+let
+  xwayland-satellite = pkgs.xwayland-satellite.overrideAttrs (oldAttrs: rec {
+    version = "0.8.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "Supreeeme";
+      repo = "xwayland-satellite";
+      tag = "v0.8.1";
+      hash = "sha256-BUE41HjLIGPjq3U8VXPjf8asH8GaMI7FYdgrIHKFMXA=";
+    };
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-16L6gsvze+m7XCJlOA1lsPNELE3D364ef2FTdkh0rVY=";
+    };
+  });
+in
 lib.mkIf config.features.desktops.niri.enable {
   programs.niri.enable = true;
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     xwayland-satellite
   ];
   # https://wiki.nixos.org/wiki/Niri
