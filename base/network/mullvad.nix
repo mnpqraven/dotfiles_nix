ed1229a0b7e7a51fb05e8cce687a86b35afb348c
Othi <mnpq.raven@gmail.com>
2026-09-23 22:24:17 +0700

    not using our own autostart

--- base/network/mullvad.nix
+++ base/network/mullvad.nix
@@ -19,21 +19,8 @@ lib.mkIf config.features.services.vpn.enable {
   # @see https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803
   services.resolved.enable = true;
 
-  systemd.services."mullvad-autostart" = lib.mkIf config.features.services.vpn.autostart {
-    description = "Mullvad autostart";
-    requires = [ "mullvad-daemon.service" ];
-    after = [ "mullvad-daemon.service" ];
-    wantedBy = [ "graphical.target" ];
-    serviceConfig = {
-      Type = "simple";
-      Restart = "on-failure";
-      RestartSec = 5;
-      StartLimitBurst = 5;
-      StartLimitIntervalSec = 60;
-      ExecStart = pkgs.writeShellScript "mullvad-autostart" ''
-        "${bin}" account login "$(cat ${account})" > /dev/null
-      '';
-    };
+  systemd.services."mullvad-daemon" = {
+    # TODO: connect if account get is null
     postStart = ''
       ${bin} lan set allow
       ${bin} lockdown-mode set on
@@ -44,4 +31,30 @@ lib.mkIf config.features.services.vpn.enable {
       ${bin} connect
     '';
   };
+
+  # systemd.services."mullvad-autostart" = lib.mkIf config.features.services.vpn.autostart {
+  #   description = "Mullvad autostart";
+  #   requires = [ "mullvad-daemon.service" ];
+  #   after = [ "mullvad-daemon.service" ];
+  #   wantedBy = [ "graphical.target" ];
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     Restart = "on-failure";
+  #     RestartSec = 5;
+  #     StartLimitBurst = 5;
+  #     StartLimitIntervalSec = 60;
+  #     ExecStart = pkgs.writeShellScript "mullvad-autostart" ''
+  #       "${bin}" account login "$(cat ${account})" > /dev/null
+  #     '';
+  #   };
+  #   postStart = ''
+  #     ${bin} lan set allow
+  #     ${bin} lockdown-mode set on
+  #     ${bin} tunnel set quantum-resistant on
+  #     ${bin} tunnel set daita on
+  #     ${bin} tunnel set daita-direct-only on
+  #     ${bin} relay set location sg
+  #     ${bin} connect
+  #   '';
+  # };
 }
