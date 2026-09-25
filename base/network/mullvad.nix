f5d4a51a26c5c84c6bf1c3970566f73646d62628
Othi <mnpq.raven@gmail.com>
2026-09-25 10:53:04 +0700

    move ssh rule block

--- base/network/mullvad.nix
+++ base/network/mullvad.nix
@@ -33,24 +33,4 @@ lib.mkIf config.features.services.vpn.enable {
       ${bin} connect
     '';
   };
-
-  networking.nftables = {
-    enable = true;
-    # ssh through vpn
-    # connection tracking mark (0x00000f41) to get through the firewall
-    # and a meta mark (0x6d6f6c65) to route the traffic outside the tunnel
-    # @see https://wiki.nftables.org/wiki-nftables/index.php/Setting_packet_metainformation#packet_mark_and_conntrack_mark
-    ruleset = ''
-      table inet excludeTraffic {
-        chain allowIncoming {
-          type filter hook input priority -100; policy accept;
-          tcp dport 22 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-        }
-        chain allowOutgoing {
-          type route hook output priority -100; policy accept;
-          tcp sport 22 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-        }
-      }
-    '';
-  };
 }
